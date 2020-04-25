d38505afc91642c6796c81f3e63ddc9ec8bacff2
Dhruv Kothari <60866749+kothariji@users.noreply.github.com>
2020-04-25 21:57:05 +0530

    Add files via upload

---
+++ day25 - one liner for loops/code.cpp
@@ -0,0 +1,13 @@
+#include<iostream>		//day25of30
+using namespace std;
+
+int main()
+{
+    int a[5];
+
+    for(int i = 0; i < 5; cin>>a[i++]);         //input: 10 20 30 40 50
+
+    for(int i = 0; i < 5; cout<<a[i++]<<" ");   //output: 10 20 30 40 50
+
+  return 0;
+}
