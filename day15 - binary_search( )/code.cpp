56097fb22352142bbc3a807c86056e47f8d156c7
Dhruv Kothari <kotharidhruv25@gmail.com>
2021-01-14 10:48:31 +0530

    Update code.cpp

--- day15 - binary_search( )/code.cpp
+++ day15 - binary_search( )/code.cpp
@@ -1,15 +1,19 @@
-#include <iostream>		//day14of30
+#include <bits/stdc++.h>    //day15of30
 using namespace std;
 
 int main(){
-
-  cout<<"Dhruv\nKothari"; 	//output: Dhruv
-  								//		  Kothari
-
-  cout<<"Dhruv"<<endl<<"Kothari"<<endl;	//output: Dhruv
-  											//		  Kothari
-  /*******endl is equiavalent to*******/
-  cout<<"\n"<<flush;
-
-  return 0;
-}
+    
+    int a[8] = {1, 7, 8, 9, 3, 5, 4, 2};
+    sort(a, a+8);
+    
+    if(binary_search(a, a+8, 2))
+        cout<<"Found";
+    else
+        cout<<"Not Found";      //output: Found
+        
+    if(binary_search(a, a+8, 6))
+        cout<<"Found";
+    else
+        cout<<"Not Found";      //output: Not Found
+    
+}   
